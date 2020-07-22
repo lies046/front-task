af0851bc71e2bd7268edcf8170f05bdeb366245f
lies046 <skytree.skysky@gmail.com>
2020-07-22 22:32:15 +0900

    Add JS and SpResponsive

---
+++ function.js
@@ -0,0 +1,33 @@
+//スクロール量を取得する関数
+function getScrolled() {
+  return (window.pageYOffset !== undefined) ? window.pageYOffset : document.documentElement.scrollTop;
+}
+
+//トップに戻るボタンの要素を取得
+var topButton = document.getElementById('top-nav');
+
+//トップに移動する関数
+function scrollToTop() {
+  var scrolled = getScrolled();
+  window.scrollTo(0, Math.floor(scrolled / 2));
+  if (scrolled > 0) {
+    window.setTimeout(scrollToTop, 30);
+  }
+};
+
+//イベント登録
+topButton.onclick = function () {
+  scrollToTop();
+};
+
+let mySwiper = new Swiper('.swiper-container', {
+  // 以下にオプションを設定
+  loop: true, //最後に達したら先頭に戻る
+
+  //ページネーション表示の設定
+  pagination: {
+    el: '.swiper-pagination', //ページネーションの要素
+    type: 'bullets', //ページネーションの種類
+    clickable: true, //クリックに反応させる
+  }
+})

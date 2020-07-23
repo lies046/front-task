f9415075a91d1a846ad5135b6ab24451b645d619
lies046 <skytree.skysky@gmail.com>
2020-07-23 11:27:04 +0900

    Add ResMenu

--- function.js
+++ function.js
@@ -31,3 +31,18 @@ let mySwiper = new Swiper('.swiper-container', {
     clickable: true, //クリックに反応させる
   }
 })
+
+//ハンバーガーメニュー 
+function toggleNav() {
+  var body = document.body;
+  var hamburger = document.getElementById('nav_btn');
+  var blackBg = document.getElementById('nav_bg');
+
+  hamburger.addEventListener('click', function () {
+    body.classList.toggle('nav_open'); //メニュークリックでnav-openというクラスがbodyに付与
+  });
+  blackBg.addEventListener('click', function () {
+    body.classList.remove('nav_open'); //もう一度クリックで解除
+  });
+}
+toggleNav();
